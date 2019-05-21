4deac8ff82dcaa9d6bccf5d8b04948fc2ab87534
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-21 17:26:43 -0400

    OOP crash course

---
+++ oop-javascript/constructor.js
@@ -0,0 +1,17 @@
+// Constructor
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+    this.getSummary = function() {
+        return `${this.title} was written by 
+        ${this.author} in ${this.year}`;
+    }
+}
+
+// Instantiate an Object
+const book1 = new Book('Book 1', 'John Doe', '2013');
+const book2 = new Book('Book 2', 'Jane Doe', '2016');
+
+console.log(book2.getSummary());
---
+++ oop-javascript/index.js
@@ -0,0 +1,30 @@
+// const s = 'hello';
+
+// const S = s.toUpperCase();
+
+// console.log(S, typeof S); // HELLO, string
+
+
+// Object Literal
+// const book1 = {
+//     title: 'Book One',
+//     author: 'John Doe',
+//     year: '2013',
+//     getSummary: function() {
+//         return `${this.title} was written by 
+//         ${this.author} in ${this.year}`;
+//     }
+// }
+
+// const book2 = {
+//     title: 'Book Two',
+//     author: 'Jane Doe',
+//     year: '2016',
+//     getSummary: function() {
+//         return `${this.title} was written by 
+//         ${this.author} in ${this.year}`;
+//     }
+// }
+
+// console.log(Object.values(book2));
+// console.log(book1.getSummary());
